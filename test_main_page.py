2e1b2d79ab8d1d53ef564b3744c3e4632349ca27
SmirnovEN <SmirnovEN@cbr.ru>
2020-05-19 18:44:41 +0300

    методы проверки ссылки на логин и возможности перехода по ней перенесены в base_page

--- test_main_page.py
+++ test_main_page.py
@@ -11,7 +11,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-def tes_guest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser):
     link = LINK
     page = MainPage(browser, link)
     page.open()
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
 import time
 import pytest
@@ -20,7 +21,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_message_book_added()
     page.should_be_basket_price_eq_book_price()
     
-@pytest.mark.xfail
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, PRODUCT_LINK)
     page.open()
@@ -33,11 +34,25 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
-@pytest.mark.xfail
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser): 
     page = ProductPage(browser, PRODUCT_LINK)
     page.open()
     page.add_product_to_basket()
     page.should_disappeared_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
     
