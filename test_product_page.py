332552e2e4db4cfb61ddd98a9fc6db9b73c99a6c
SmirnovEN <SmirnovEN@cbr.ru>
2020-05-19 18:23:39 +0300

    add 3 simple neg test

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
 import time
 import pytest
 
-def test_guest_can_add_product_to_basket(browser):
+PRODUCT_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+#def test_guest_can_add_product_to_basket(browser):
+def tst_guest_can_add_product_to_basket(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -15,6 +19,25 @@ def test_guest_can_add_product_to_basket(browser):
 
     page.should_be_message_book_added()
     page.should_be_basket_price_eq_book_price()
-
     
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, PRODUCT_LINK)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, PRODUCT_LINK)
+    page.open()
+    page.should_not_be_success_message()
+    
+
+def test_message_disappeared_after_adding_product_to_basket(browser): 
+    page = ProductPage(browser, PRODUCT_LINK)
+    page.open()
+    page.add_product_to_basket()
+    page.should_disappeared_success_message()
+
     
