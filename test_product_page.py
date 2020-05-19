8afaba58b20e2ea16c2705b83bbc9ec88a55ff38
SmirnovEN <SmirnovEN@cbr.ru>
2020-05-19 18:29:15 +0300

    Вернул обратно тест, убранный для ускорения прогона

--- test_product_page.py
+++ test_product_page.py
@@ -6,7 +6,7 @@ import pytest
 PRODUCT_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 #def test_guest_can_add_product_to_basket(browser):
-def tst_guest_can_add_product_to_basket(browser):
+def test_guest_can_add_product_to_basket(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
